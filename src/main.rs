073e555e9dd8552d5e08f5099c1838a4dcfdee1a
Julian V <jvesper95@gmail.com>
2022-01-12 16:35:06 +0100

    Add basic parsing for turnovers

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,43 @@
-//tutorial-setup-01.rs
-// Import the standard library's I/O module so we can read from stdin.
 use std::io;
 
+use serde::{de, Deserialize, Deserializer};
+
+#[derive(Debug, Deserialize)]
+struct Turnover {
+	// TODO: Convert to dates
+	day: String,
+	day_2: String,
+	operation: String,
+	// TODO: Convert to custom struct
+	description: String,
+	#[serde(deserialize_with ="deserialize_comma_float")]
+	value: f64,
+}
+
 fn main() {
+	// TODO: Format csv file
     // Create a CSV parser that reads data from stdin.
-    let mut reader = csv::Reader::from_reader(io::stdin());
-    // Loop over each record.
-    for result in reader.records() {
-        // An error may occur, so abort the program in an unfriendly way.
-        // We will make this more friendly later!
-        let record = result.expect("a CSV record");
-        // Print a debug version of the record.
-        println!("{:?}", record);
-    }
+    let mut reader = csv::ReaderBuilder::new()
+		.has_headers(false)
+		.delimiter(b';')
+		.from_reader(io::stdin());
+
+	// Get all turnovers
+	let turnovers: Vec<Turnover> = reader.deserialize()
+		.map(|t| t.unwrap())
+		.collect();
+
+	dbg!(turnovers);
+}
+
+// Custom deserializer for floats in European float format (e.g.: 24.016,56)
+fn deserialize_comma_float<'de, D>(deserializer: D) -> Result<f64, D::Error> where D: Deserializer<'de> {
+	let buf = String::deserialize(deserializer)?;
+
+	// Remove dots and replace commas with dots
+	buf
+		.replace('.', "")
+		.replace(',', ".")
+		.parse()
+		.map_err(de::Error::custom)
 }
