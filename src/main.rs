04c5c708bd4cf0489420fe38a602fe1932cd582b
Julian V <jvesper95@gmail.com>
2022-05-15 15:01:18 +0200

    Add correct file encoding so the file does not need to be converted beforehand

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,10 @@ use std::{fs::File, io::BufReader};
 use anyhow::Context;
 use chrono::NaiveDate;
 use clap::Parser;
+use encoding_rs::ISO_8859_15;
+use encoding_rs_io::DecodeReaderBytesBuilder;
 use serde::Deserialize;
 
-use crate::crossterm::run;
 use custom_formats::{custom_date_format, custom_float_format};
 
 #[derive(Debug, Deserialize)]
@@ -50,11 +51,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Read both files and handle errors
     let first_account_file = File::open(&args.file1)
         .with_context(|| format!("could not read file {:?}", &args.file1))?;
-    let first_account_reader = BufReader::new(first_account_file);
+    let first_account_reader = BufReader::new(
+        DecodeReaderBytesBuilder::new()
+            .encoding(Some(ISO_8859_15))
+            .build(first_account_file),
+    );
 
     let second_account_file = File::open(&args.file2)
         .with_context(|| format!("could not read file {:?}", &args.file2))?;
-    let second_account_reader = BufReader::new(second_account_file);
+    let second_account_reader = BufReader::new(
+        DecodeReaderBytesBuilder::new()
+            .encoding(Some(ISO_8859_15))
+            .build(second_account_file),
+    );
 
     // Create a CSV parser that reads data from stdin.
     let mut reader = csv::ReaderBuilder::new()
@@ -71,12 +80,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     //dbg!(first_account_turnovers);
     //dbg!(second_account_turnovers);
 
-    // TODO: Convert file from ANSI to UTF-8 encoding
     // TODO: Preformat csv file
     // TODO: Add (f)ilter
     // TODO: Add (a)nalyze functionality
 
-    run(tick_rate)?;
+    //run(tick_rate)?;
 
     Ok(())
 }
--- src/ui.rs
+++ src/ui.rs
@@ -8,18 +8,12 @@ use tui::{
 use crate::app::App;
 
 pub fn draw<B: Backend>(f: &mut Frame<B>, app: &mut App) {
+    // Create main layout
     let chunks = Layout::default()
-        .direction(Direction::Vertical)
-        .margin(1)
-        .constraints(
-            [
-                Constraint::Percentage(10),
-                Constraint::Percentage(80),
-                Constraint::Percentage(10),
-            ]
-            .as_ref(),
-        )
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
         .split(f.size());
+
     let block = Block::default().title("Block").borders(Borders::ALL);
     f.render_widget(block, chunks[0]);
     let block = Block::default().title("Block 2").borders(Borders::ALL);
